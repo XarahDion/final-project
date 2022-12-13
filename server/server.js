ff9c2dea284709dbd00ff18c8dcd1a32dc0c9194
Xarah Dion <xarahdion@gmail.com>
2022-12-12 19:30:49 -0500

    server change

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors")
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8000;
 
 const {
     getConcerts,
